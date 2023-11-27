d851ccd3561420d9e93437bde168368a67bd3040
Jovenne Naldrey Sapuay <jovenneregs@gmail.com>
2023-11-27 20:59:27 +0800

    Change port 5000 to 3000: issue on cyclic logs

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const connectDB = require('./config/db');
 
 //require('dotenv').config();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
